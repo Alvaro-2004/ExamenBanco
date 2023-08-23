7c2fc203a1794c7a483b92ae65016eb66d9cc9fc
Alvaro-2004 <alvaro09ar@icloud.com>
2023-08-23 09:12:06 -0600

    Creacion MVC
    Se crean los paquetes MVC

---
+++ src/Controller/EnumTipo.java
@@ -0,0 +1,11 @@
+package Controller;
+
+/**
+ *
+ * @author Student
+ */
+public enum EnumTipo {
+    Plataformas,
+    Caja,
+    Preferencial;
+}
---
+++ src/Models/Tickets.java
@@ -0,0 +1,12 @@
+package Models;
+
+import Controller.EnumTipo;
+
+/**
+ *
+ * @author Student
+ */
+public class Tickets {
+    private int numero;
+    private EnumTipo tipo;
+}
