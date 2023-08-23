4634f365c30beb631607a3c6a63e74f31896dfbd
Alvaro-2004 <alvaro09ar@icloud.com>
2023-08-23 10:22:39 -0600

    Clase TicketsList
    Se crea la clase, y se programan metodos

--- src/Models/Tickets.java
+++ src/Models/Tickets.java
@@ -7,6 +7,42 @@ import Controller.EnumTipo;
  * @author Student
  */
 public class Tickets {
+    public static int numP = 0;
+    public static int numC = 0;
+    public static int numA = 0;
     private int numero;
     private EnumTipo tipo;
+
+    public Tickets(EnumTipo tipo) {
+        this.tipo = tipo;
+        this.numAuto();
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public EnumTipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(EnumTipo tipo) {
+        this.tipo = tipo;
+    }
+    
+    public void numAuto(){
+        switch (this.tipo){
+            case Plataformas:
+                numP++;
+            case Caja:
+                numC++;
+            case Preferencial:
+                numA++;
+        }
+    }
+    
 }
